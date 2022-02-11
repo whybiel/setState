7cf97de2468d496a328d20f4897cef93050a8cc9
gabrielvnw <gabrimoraes2004@gmail.com>
2022-02-11 18:51:39 -0300

    Aula setState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class State extends Component{
+
+  state={
+    nome:"Gabriel",
+    idade:17,
+    cor:"Azul"
+  }
+
+  mensagem = () => {
+    if (this.state.cor === "Azul"){
+      this.setState({
+        cor:"Amarelo",
+        nome:"Henrique"
+      })
+    }else if(this.state.cor === "Amarelo"){
+      this.setState({
+        cor:"Preto",
+        nome:"Ferreira"
+      })
+    }else{
+      this.setState({
+        cor:"Azul",
+        nome:"Moraes"
+      })
+    }
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Minha cor favorita é {this.state.cor} e meu nome é {this.state.nome}</h1>
+        <button onClick={this.mensagem}>Trocar</button>
+      </div>
+    )
+  }
 }
-
-export default App;
